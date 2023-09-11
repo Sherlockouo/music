e1b8d31a32ca34dd53e6b37ac1f529a2ea7fcc32
wudingfeng <wdf.coder@gmail.com>
2023-09-11 22:31:45 +0800

    -a

--- packages/web/components/LayoutMobile.tsx
+++ packages/web/components/LayoutMobile.tsx
@@ -1,6 +1,6 @@
 import Player from '@/web/components/PlayerMobile'
 import { css, cx } from '@emotion/css'
-import { useMemo } from 'react'
+import { startTransition, useMemo } from 'react'
 import player from '@/web/states/player'
 import { useSnapshot } from 'valtio'
 import Router from '@/web/components/Router'
@@ -16,49 +16,50 @@ const LayoutMobile = () => {
   const showPlayer = !!playerSnapshot.track
   const location = useLocation()
 
-  return (
-    <div id='layout' className='select-none bg-white pb-28 dark:bg-black'>
-      <main className='min-h-screen overflow-y-auto overflow-x-hidden pb-16 '>
-        {location.pathname === '/' && <Topbar />}
-        <Router />
-      </main>
-      <div
-        className={cx('fixed bottom-0 left-0 right-0 z-20 pt-3 dark:bg-black')}
-        style={{
-          paddingBottom: `calc(
+  return
+  // startTransition(() => {
+  ;<div id='layout' className='select-none bg-white pb-28 dark:bg-black'>
+    <main className='min-h-screen overflow-y-auto overflow-x-hidden pb-16 '>
+      {location.pathname === '/' && <Topbar />}
+      <Router />
+    </main>
+    <div
+      className={cx('fixed bottom-0 left-0 right-0 z-20 pt-3 dark:bg-black')}
+      style={{
+        paddingBottom: `calc(
               ${isIosPwa ? '24px' : 'env(safe-area-inset-bottom)'} + 0.75rem
             )`,
-        }}
-      >
-        {showPlayer && (
-          <div
-            className={cx('absolute left-7 right-7 z-20')}
-            style={{
-              top: `calc(-100% - 6px + ${isIosPwa ? '24px' : 'env(safe-area-inset-bottom)'})`,
-            }}
-          >
-            <Player />
-          </div>
-        )}
-
-        <MenuBar />
-        <PlayingNext />
-      </div>
-      <Login />
-      {/* Notch background */}
-      {isIosPwa && (
+      }}
+    >
+      {showPlayer && (
         <div
-          className={cx(
-            'fixed left-0 right-0 bg-black/30 backdrop-blur-sm',
-            css`
-              top: -50px;
-              height: 50px;
-            `
-          )}
-        ></div>
+          className={cx('absolute left-7 right-7 z-20')}
+          style={{
+            top: `calc(-100% - 6px + ${isIosPwa ? '24px' : 'env(safe-area-inset-bottom)'})`,
+          }}
+        >
+          <Player />
+        </div>
       )}
+
+      <MenuBar />
+      <PlayingNext />
     </div>
-  )
+    <Login />
+    {/* Notch background */}
+    {isIosPwa && (
+      <div
+        className={cx(
+          'fixed left-0 right-0 bg-black/30 backdrop-blur-sm',
+          css`
+            top: -50px;
+            height: 50px;
+          `
+        )}
+      ></div>
+    )}
+  </div>
+  // })
 }
 
 export default LayoutMobile

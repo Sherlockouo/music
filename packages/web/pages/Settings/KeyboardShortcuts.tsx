11a6b6e4b7b9d7349ec2036cd581c61d089cd1fb
Ivan Li <ivanli2048@gmail.com>
2023-10-15 15:34:26 +0800

    feat(web/settings): keyboard shortcuts setting and saving.

--- packages/web/pages/Settings/KeyboardShortcuts.tsx
+++ packages/web/pages/Settings/KeyboardShortcuts.tsx
@@ -1,9 +1,13 @@
-import { t } from 'i18next'
-import { FC } from 'react'
-import settings, { KeyboardShortcuts } from '@/web/states/settings'
-import useSettings from '@/web/hooks/useSettings'
+import Icon from '@/web/components/Icon'
 import useKeyboardShortcuts from '@/web/hooks/useKeyboardShortcuts'
-import { BlockTitle, OptionText, Option, Switch } from './Controls'
+import useOSPlatform from '@/web/hooks/useOSPlatform'
+import useSettings from '@/web/hooks/useSettings'
+import settings, { KeyboardShortcuts } from '@/web/states/settings'
+import { t } from 'i18next'
+import { toString as event2String } from 'keyboard-event-to-string'
+import { FC, KeyboardEventHandler, useEffect, useMemo, useState } from 'react'
+import toast from 'react-hot-toast'
+import { BlockTitle, Option, OptionText, Switch } from './Controls'
 
 const ShortcutSwitchSettings = () => {
   const {
@@ -28,9 +32,87 @@ const ShortcutBindingInput: FC<{
   value: string | null
   onChange?: (value: string | null) => void
 }> = ({ value, onChange }) => {
+  const [inputValue, setInputValue] = useState(value)
+  const [isBinding, setBinding] = useState(false)
+
+  useEffect(() => {
+    setInputValue(value)
+  }, [value])
+
+  const text = useMemo(() => {
+    if (isBinding && inputValue === null) {
+      return t`settings.keyboard-shortcuts.binding-input-waiting-keydown-placeholder`
+    }
+
+    if (inputValue === null) {
+      return ''
+    }
+
+    return inputValue
+  }, [inputValue, isBinding])
+
+  const clear = () => {
+    setInputValue(null)
+    onChange?.(null)
+  }
+
+  const startBinding = () => {
+    setInputValue(null)
+    setBinding(true)
+  }
+
+  const onKeyDown: KeyboardEventHandler = e => {
+    if (!isBinding) {
+      return
+    }
+
+    if (e.key === 'Escape') {
+      clear()
+      return
+    }
+
+    if (e.key === 'Enter') {
+      onChange?.(inputValue)
+      setBinding(false)
+      return
+    }
+
+    const text = event2String(e as any) // React keyboard event -> DOM keyboard event
+    setInputValue(text)
+  }
+
+  const onBlur = () => {
+    if (!isBinding) {
+      return
+    }
+
+    setInputValue(value)
+    setBinding(false)
+
+    if (inputValue !== null) {
+      onChange?.(inputValue)
+    }
+  }
+
   return (
-    <div className='mx-2 rounded-lg bg-stone-400/20 py-1 px-3 font-mono backdrop-blur '>
-      {value}
+    <div
+      className='group/binding-input mx-2 flex gap-2 rounded-lg bg-stone-400/20 py-1 px-3 font-mono outline-none backdrop-blur'
+      onClick={startBinding}
+      onKeyDown={onKeyDown}
+      onBlur={onBlur}
+      tabIndex={0}
+    >
+      <span>{text}</span>
+      <button
+        onClick={clear}
+        className='ml-auto'
+        title={t`settings.keyboard-shortcuts.item-clear`!}
+      >
+        <Icon
+          name='x'
+          className='ml-auto box-content h-4 w-4 rounded-full p-1 text-stone-300 opacity-0 transition-opacity hover:bg-stone-400/20 group-hover/binding-input:opacity-100 dark:text-stone-400'
+        />
+      </button>
     </div>
   )
 }
@@ -40,17 +122,30 @@ const ShortcutItemBindings: FC<{ fnKey: keyof KeyboardShortcuts; name: string }>
   name,
 }) => {
   const keyboardShortcuts = useKeyboardShortcuts()
+  const platform = useOSPlatform()
 
-  console.log(keyboardShortcuts, fnKey)
+  const updateBinding = (index: 0 | 1) => {
+    return (value: string | null) => {
+      const conflicted =
+        value !== null && Object.values(settings.keyboardShortcuts[platform]).flat().includes(value)
+
+      if (conflicted) {
+        toast.error(t`settings.keyboard-shortcuts.keyboard-shortcut-in-used`)
+        return
+      }
+
+      settings.keyboardShortcuts[platform][fnKey][index] = value
+    }
+  }
 
   return (
     <tr className='h-10 rounded-lg hover:bg-stone-100/20 hover:dark:bg-stone-600/10'>
       <td className='px-2 text-left font-normal'>{name}</td>
       <td className='text-center font-normal'>
-        <ShortcutBindingInput value={keyboardShortcuts[fnKey][0]} />
+        <ShortcutBindingInput value={keyboardShortcuts[fnKey][0]} onChange={updateBinding(0)} />
       </td>
       <td className='text-center font-normal'>
-        <ShortcutBindingInput value={keyboardShortcuts[fnKey][1]} />
+        <ShortcutBindingInput value={keyboardShortcuts[fnKey][1]} onChange={updateBinding(1)} />
       </td>
     </tr>
   )
